ecfbc71a30269c25c8169183b7094a02560928af
NextZeus <875697126@qq.com>
2017-09-25 21:57:21 +0800

    NavMeshAgent

---
+++ Assets/Scripts/Helpers/Navigation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class Navigation : MonoBehaviour 
+{
+	
+	[SerializeField] Transform target;
+	[SerializeField] NavMeshAgent agent;
+	[SerializeField] float updateDelay = .3f;
+
+	void Reset()
+	{
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+
+	void Start()
+	{
+		// agent.SetDestination(target.position);
+		InvokeRepeating("FollowTarget",0f,updateDelay);
+	}
+		
+	// void Update()
+	// {
+	// 	agent.SetDestination(target.position);		
+	// }
+
+	void FollowTarget()
+	{
+		agent.SetDestination(target.position);
+	}
+}
